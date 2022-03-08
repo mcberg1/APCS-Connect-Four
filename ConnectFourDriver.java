8006fea0771e0029548bff559fb921fe9939aa68
Matthew Berg <bob8898@outlook.com>
2022-03-08 15:12:50 -0700

    Add files via upload

--- ConnectFourDriver.java
+++ ConnectFourDriver.java
@@ -1,14 +1,26 @@
 package ConnFour;
 
+import java.util.Scanner;
+import java.util.Date;
 public class ConnectFourDriver {
 
     public static void main(String[] args) {
         ConnectFourGame game = new ConnectFourGame();
-        while (true) {
-            game.generateRandomBoard();
+        Scanner input = new Scanner(System.in);
+
+        while (game.isRunning()) {
             game.printBoard();
-            game.clearBoard();
-            delay(100);
+            System.out.print("Player " + (game.getTurn() ? "2" : "1") + " turn, which column?");
+            int selectedColumn = input.nextInt();
+            if (selectedColumn > game.numColumns() || selectedColumn < 1)
+                for (int i = 0; i < 100; i++)
+                    System.out.println("bruh");
+            else {
+                if (!game.addDisc(selectedColumn-1, game.getTurn()))
+                    System.out.println("Column " + selectedColumn + " full");
+                    game.toggleTurn();
+            }
+            //delay(1000);
         }
     }
 
--- ConnectFourGame.java
+++ ConnectFourGame.java
@@ -1,21 +1,16 @@
 package ConnFour;
 
-public class ConnectFourGame {
-    Disc[][] Board = new Disc[6][7];
 
-    /*
-    {disc, disc, disc, disc, disc, disc}
-    {disc, disc, disc, disc, disc, disc}
-    {disc, disc, disc, disc, disc, disc}
-    {disc, disc, disc, disc, disc, disc}
-    {disc, disc, disc, disc, disc, disc}
-    {disc, disc, disc, disc, disc, disc}
-    {disc, disc, disc, disc, disc, disc}
-    */
+public class ConnectFourGame {
+    private Disc[][] Board = new Disc[6][7];
+    private int Places[] = new int[Board[0].length];
+    private boolean turn = false;
+    private boolean running = true;
+    private int toWin = 4;
     ConnectFourGame() {
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 7; j++) {
-                Board[i][j] = new Disc();
+                Board[i][j] = new Disc(discArrayHelper(i, j - 1), discArrayHelper(i, j + 1), discArrayHelper(i - 1, j), discArrayHelper(i - 1, j - 1), discArrayHelper(i - 1, j + 1));
             }
         }
     }
@@ -29,15 +24,21 @@ public class ConnectFourGame {
     }
 
     public void printBoard() { //Print 2d array upside down.
-        for(int i = 0; i < 10; i++)
+        for (int i = 0; i < 5; i++)
             System.out.println();
+        for (int i = 0; i < 30; i++)
+            System.out.print("-");
+        System.out.println();
         for (int i = Board.length - 1; i >= 0; i--) {
             for (Disc disc : Board[i]) {
+                System.out.print("| ");
                 System.out.print(disc.isPlayerSet() ? disc.getPlayer() ? "x" : "o" : " ");
+                System.out.print(" ");
             }
             System.out.println();
         }
     }
+
     public void clearBoard() {
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 7; j++) {
@@ -45,4 +46,72 @@ public class ConnectFourGame {
             }
         }
     }
+
+    public boolean addDisc(int col, boolean player) {
+        if (Places[col] >= Board.length) {
+            //System.out.println("Column " + col + " is full...");
+            return false;
+        }
+        Board[Places[col]][col].setPlayer(player);
+        checkWin(Places[col], col);
+        Places[col]++;
+
+        return true;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public boolean getTurn() {
+        return turn;
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+    public int numColumns() {
+        return Board[0].length;
+    }
+
+    public int numRows() {
+        return Board.length;
+    }
+
+    public void toggleTurn() {
+        turn = !turn;
+    }
+
+    public Disc discArrayHelper(int i, int j) {
+        try {
+            return Board[i][j];
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public boolean checkWin(int i, int j) {
+        Disc checking = Board[i][j];
+        int initialDir = checking.checkAllAdjacentDiscs(0);
+        if (initialDir != -1) {
+            for (int dir = initialDir; dir < 5; dir++) {
+                try {
+                    for (int k = 0; k < toWin - 1; k++)
+                        checking = checking.checkAdjacentDisc(dir);
+                    if (checking._playerSet) {
+                        System.out.println("win");
+                        return true;
+                    }
+                } catch (Exception e) {
+                    System.out.println("no win");
+                    return false;
+                }
+            }
+        }
+        System.out.println("no neighbors");
+        return false;
+
+    }
+
 }
